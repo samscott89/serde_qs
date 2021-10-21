f35013ac17eeec7f771650a3c9cf2124ba9bff8b
Omid Rad <omidmr@gmail.com>
2021-10-21 12:38:48 +0200

    Upgrade actix version

--- src/actix.rs
+++ src/actix.rs
@@ -13,7 +13,9 @@ use actix_web3 as actix_web;
 use actix_web2 as actix_web;
 
 use actix_web::dev::Payload;
-use actix_web::{Error as ActixError, FromRequest, HttpRequest, HttpResponse, ResponseError};
+use actix_web::{Error as ActixError, FromRequest, HttpRequest, ResponseError};
+#[cfg(any(feature = "actix2", feature = "actix3"))]
+use actix_web::HttpResponse;
 use futures::future::{ready, Ready};
 use serde::de;
 use std::fmt;
@@ -57,7 +59,9 @@ impl ResponseError for QsError {
 /// // Use `QsQuery` extractor for query information.
 /// // The correct request for this handler would be `/users?id[]=1124&id[]=88"`
 /// fn filter_users(info: QsQuery<UsersFilter>) -> HttpResponse {
-///     info.id.iter().map(|i| i.to_string()).collect::<Vec<String>>().join(", ").into()
+///     HttpResponse::Ok().body(
+///         info.id.iter().map(|i| i.to_string()).collect::<Vec<String>>().join(", ")
+///     )
 /// }
 ///
 /// fn main() {
@@ -107,6 +111,7 @@ where
 {
     type Error = ActixError;
     type Future = Ready<Result<Self, ActixError>>;
+    #[cfg(any(feature = "actix2", feature = "actix3"))]
     type Config = QsQueryConfig;
 
     #[inline]
@@ -142,11 +147,14 @@ where
 /// # #[macro_use] extern crate serde_derive;
 /// # #[cfg(feature = "actix")]
 /// # use actix_web;
+/// # #[cfg(feature = "actix3")]
+/// # use actix_web3 as actix_web;
 /// # #[cfg(feature = "actix2")]
 /// # use actix_web2 as actix_web;
 /// use actix_web::{error, web, App, FromRequest, HttpResponse};
 /// use serde_qs::actix::QsQuery;
 /// use serde_qs::Config as QsConfig;
+/// use serde_qs::actix::QsQueryConfig;
 ///
 /// #[derive(Deserialize)]
 /// struct Info {
@@ -155,20 +163,21 @@ where
 ///
 /// /// deserialize `Info` from request's querystring
 /// fn index(info: QsQuery<Info>) -> HttpResponse {
-///     format!("Welcome {}!", info.username).into()
+///     HttpResponse::Ok().body(
+///         format!("Welcome {}!", info.username)
+///     )
 /// }
 ///
 /// fn main() {
-///     let app = App::new().service(
-///         web::resource("/index.html").app_data(
-///             // change query extractor configuration
-///             QsQuery::<Info>::configure(|cfg| {
-///                 cfg.error_handler(|err, req| {  // <- create custom error response
-///                     error::InternalError::from_response(
-///                         err, HttpResponse::Conflict().finish()).into()
-///                 })
-///                 .qs_config(QsConfig::default())
-///             }))
+/// let qs_config = QsQueryConfig::default()
+///     .error_handler(|err, req| {  // <- create custom error response
+///     error::InternalError::from_response(
+///         err, HttpResponse::Conflict().finish()).into()
+///     })
+///     .qs_config(QsConfig::default());
+///
+/// let app = App::new().service(
+///         web::resource("/index.html").app_data(qs_config)
 ///             .route(web::post().to(index))
 ///     );
 /// }
